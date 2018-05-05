33d60da8b1677936ea88da694fa36953366b7a7a
ikuhiroo <i2ku.nn@gmail.com>
2018-05-05 23:57:43 +0900

    change class label

--- Perceptron.java
+++ Perceptron.java
@@ -2,25 +2,33 @@ public class Perceptron{
     //入力パターン
     final static double X[] = {1.2, 0.2, -0.2, -0.5, -1.0, -1.5};
     //正解ラベル
-    final static int target[] = {1, 1, 1, -1, -1, -1};
+    final static int target[] = {1, 1, 1, 0, 0, 0};
     //学習率
     final static double eta = 1.2;
-    //iter
+    //学習繰り返し回数
     final static int iter = 100;
-    //重みベクトル
-    static double w[] = {-7, 2};
     //error
     static int error = 0;
+    //重みベクトル[w0, w1]
+    static double w[] = {-7, 2};
 
     public static void main(String[] args){
-        //初期化の確認
-        String init_txt = String.format("w0=%.1f, w1=%.1f, eta=%.1f, iter=%d", w[0], w[1], eta, iter);
-        System.out.println(init_txt);
+        //init
+        for(int xi = 0; xi < X.length; xi++){
+            int haserror = target[xi] - predict(xi);
+            if(haserror != 0){
+               error += 1;
+            }
+        }
+        String hyparameter = String.format("hyparameter : eta=%.1f, iter=%d, w0=%.1f, w1=%.1f", eta, iter, w[0], w[1]);
+        System.out.println(hyparameter);
+        String init_result = String.format("0epoch : 誤分類数=%d", error);
+        System.out.println(init_result);
 
         //epochを回す
-        for(int epoch = 0; epoch < iter; epoch++){
+        for(int epoch = 1; epoch < iter; epoch++){
             error = 0;
-            update();
+            update(epoch);
             if(error == 0){
                 break;
             }
@@ -28,19 +36,22 @@ public class Perceptron{
     }
 
     //学習
-    public static void update(){
+    public static void update(int epoch){
         //バッチ処理
         for(int xi = 0; xi < X.length; xi++){
             //誤分類の確認
             double haserror = eta * (target[xi] - predict(xi));
             if(haserror != 0){
-               error += 1;
+                //errorの更新
+                error += 1;
+                //wの更新
+                w[0] = w[0] + haserror;
+                w[1] = w[1] + (haserror * X[xi]);
+                String result = String.format("w0=%.1f, w1=%.1f", w[0], w[1]);
+                System.out.println(result);
             }
-            //wの更新
-            w[0] = w[0] + haserror;
-            w[1] = w[1] + (haserror * X[xi]);
         }
-        String result = String.format("誤分類数=%d, w0=%.1f, w1=%.1f", error, w[0], w[1]);
+        String result = String.format("%depoch : 誤分類数=%d", epoch, error);
         System.out.println(result);
     }
 
@@ -52,7 +63,7 @@ public class Perceptron{
         if(pred >= 0){
            return 1;
         }else{
-           return -1;
+           return 0;
         }
     }
 }
